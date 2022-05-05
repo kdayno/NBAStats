de956ca81701496c44bda62c067bdb26bf788a63
Kevin Dayno <kdayno@gmail.com>
2022-05-05 13:28:30 -0400

    Modify script to include schema name

--- src/ingest_api_to_asqldb.py
+++ src/ingest_api_to_asqldb.py
@@ -20,48 +20,48 @@ api = api.NBAStatsAPI()
 
 print("Data ingestion starting...")
 
-# LOAD DATA INTO LANDING_TEAMS TABLE
+# LOAD DATA INTO LANDING.TEAMS TABLE
 teams_data_df = pd.DataFrame(api.team_data)
 
 with engine.connect() as connection:
-    teams_data_df.to_sql('landing_teams', connection,
+    teams_data_df.to_sql('teams', connection, schema='landing',
                          if_exists='append', index=False, dtype=team_column_dtypes)
 
-# LOAD DATA INTO LANDING_PLAYER_PROFILES TABLE
+# LOAD DATA INTO LANDING.PLAYER_PROFILES TABLE
 player_profiles_data_df = pd.DataFrame(api.player_profile_data)
 player_profiles_data_df.drop(
     columns=['teamSitesOnly', 'teams', 'draft'], inplace=True)
 
 with engine.connect() as connection:
-    player_profiles_data_df.to_sql('landing_player_profiles', connection,
+    player_profiles_data_df.to_sql('player_profiles', connection, schema='landing',
                                    if_exists='append', index=False, chunksize=100, dtype=player_profile_column_dtypes)
 
-# LOAD DATA INTO LANDING_SEASON_SCHEDULE TABLE
+# LOAD DATA INTO LANDING.SEASON_SCHEDULE TABLE
 schedule_data_df = pd.DataFrame(api.season_schedule_data)
 schedule_data_df.drop(
     columns=['period', 'nugget', 'hTeam', 'vTeam', 'watch', 'playoffs', 'tags', 'playoffs_vTeam', 'playoffs_hTeam'], inplace=True)
 
 with engine.connect() as connection:
-    schedule_data_df.to_sql('landing_season_schedule', connection,
+    schedule_data_df.to_sql('season_schedule', connection, schema='landing',
                             if_exists='append', index=False, chunksize=100, dtype=season_schedule_column_dtypes)
 
-# LOAD DATA INTO LANDING_PLAYER_CAREER_STATS TABLE
+# LOAD DATA INTO LANDING.PLAYER_CAREER_STATS TABLE
 player_career_stats_df = pd.DataFrame(api.get_all_players_career_stats_data())
 
 with engine.connect() as connection:
     player_career_stats_df.to_sql(
-        'landing_player_career_stats', connection, if_exists='append', index=False, chunksize=100, dtype=player_career_stats_column_dtypes)
+        'player_career_stats', connection, schema='landing', if_exists='append', index=False, chunksize=100, dtype=player_career_stats_column_dtypes)
 
-# LOAD DATA INTO LANDING_BOX_SCORE_BASIC_GAME_STATS TABLE
+# LOAD DATA INTO LANDING.BOX_SCORE_BASIC_GAME_STATS TABLE
 box_score_basic_game_stats_df = pd.DataFrame(
     api.get_all_box_score_basic_game_stats_data())
 
 with engine.connect() as connection:
     box_score_basic_game_stats_df.to_sql(
-        'landing_box_score_basic_game_stats', connection, if_exists='replace', index=False, chunksize=100
+        'box_score_basic_game_stats', connection, schema='landing', if_exists='replace', index=False, chunksize=100
     )
 
-# LOAD DATA INTO LANDING_BOX_SCORE_DETAILED_GAME_STATS TABLES
+# LOAD DATA INTO LANDING.BOX_SCORE_DETAILED_GAME_STATS TABLES
 # Data has to be split into 3 tables due to MS SQL Server's 1024 column limit per table
 box_score_detailed_game_stats_df = pd.DataFrame(
     api.get_all_box_score_detailed_game_stats_data()
@@ -86,17 +86,17 @@ box_score_detailed_game_stats_df_3.insert(0, 'gameId', game_ids)
 
 with engine.connect() as connection:
     box_score_detailed_game_stats_df_1.to_sql(
-        'landing_box_score_detailed_game_stats_1', connection, if_exists='replace', index=False, chunksize=1000
+        'box_score_detailed_game_stats_1', connection, schema='landing', if_exists='replace', index=False, chunksize=1000
     )
 
 with engine.connect() as connection:
     box_score_detailed_game_stats_df_2.to_sql(
-        'landing_box_score_detailed_game_stats_2', connection, if_exists='replace', index=False, chunksize=1000
+        'box_score_detailed_game_stats_2', connection, schema='landing', if_exists='replace', index=False, chunksize=1000
     )
 
 with engine.connect() as connection:
     box_score_detailed_game_stats_df_3.to_sql(
-        'landing_box_score_detailed_game_stats_3', connection, if_exists='replace', index=False, chunksize=1000
+        'box_score_detailed_game_stats_3', connection, schema='landing', if_exists='replace', index=False, chunksize=1000
     )
 
 print("Data ingestion complete.")
