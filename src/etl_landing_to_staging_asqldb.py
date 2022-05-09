6582f9330e759e0f4bb8ab31c7ae0d88d95eda90
Kevin Dayno <kdayno@gmail.com>
2022-05-09 16:35:24 -0400

    Update landing to staging ETL

--- src/etl_landing_to_staging_asqldb.py
+++ src/etl_landing_to_staging_asqldb.py
@@ -101,7 +101,130 @@ with engine.connect() as connection:
                               con=connection, schema='staging', if_exists='append', index=False)
 
 
-# landing_player_profiles
-# landing_player_career_stats
+# ETL DATA FROM "landing.player_profiles" TO "staging.player_profiles"
+with engine.connect() as connection:
+
+    selected_columns = ['firstName', 'lastName', 'temporaryDisplayName', 'personId', 'teamId', 'jersey',
+                        'pos', 'heightFeet', 'heightInches', 'heightMeters', 'weightPounds', 'weightKilograms',
+                        'dateOfBirthUTC', 'nbaDebutYear', 'yearsPro', 'collegeName', 'lastAffiliation',
+                        'country', 'teamSitesOnly_playerCode', 'teamSitesOnly_posFull', 'draft_teamId',
+                        'draft_pickNum', 'draft_roundNum', 'draft_seasonYear', 'teamhist_1_teamId',
+                        'teamhist_1_seasonStart', 'teamhist_1_seasonEnd', 'teamhist_2_teamId',
+                        'teamhist_2_seasonStart', 'teamhist_2_seasonEnd', 'teamhist_3_teamId',
+                        'teamhist_3_seasonStart', 'teamhist_3_seasonEnd']
+
+    player_profiles_df = pd.read_sql_table(
+        table_name='player_profiles', con=connection, schema='landing', columns=selected_columns)
+
+    player_profiles_df = player_profiles_df[selected_columns]
+
+    rename_column_mapping = {'firstName': 'PlayerFirstName',
+                             'lastName': 'PlayerLastName',
+                             'temporaryDisplayName': 'PlayerFullName',
+                             'personId': 'PersonID',
+                             'teamId': 'TeamID',
+                             'jersey': 'JerseyNumber',
+                             'pos': 'PlayerPositionCode',
+                             'heightFeet': 'HeightFeet',
+                             'heightInches': 'HeightInches',
+                             'heightMeters': 'HeightMeters',
+                             'weightPounds': 'WeightPounds',
+                             'weightKilograms': 'WeightKilograms',
+                             'dateOfBirthUTC': 'BirthDateUTC',
+                             'nbaDebutYear': 'NBADebutYear',
+                             'yearsPro': 'YearsProCount',
+                             'collegeName': 'CollegeName',
+                             'lastAffiliation': 'LastAffiliation',
+                             'country': 'CountryName',
+                             'teamSitesOnly_playerCode': 'PlayerAlternativeFullName',
+                             'teamSitesOnly_posFull': 'PlayerPositionName',
+                             'draft_teamId': 'DraftTeamID',
+                             'draft_pickNum': 'DraftPickNumber',
+                             'draft_roundNum': 'DraftRoundNumber',
+                             'draft_seasonYear': 'DraftSeasonYear',
+                             'teamhist_1_teamId': 'PreviousTeam1ID',
+                             'teamhist_1_seasonStart': 'PreviousTeam1StartYear',
+                             'teamhist_1_seasonEnd': 'PreviousTeam1EndYear',
+                             'teamhist_2_teamId': 'PreviousTeam2ID',
+                             'teamhist_2_seasonStart': 'PreviousTeam2StartYear',
+                             'teamhist_2_seasonEnd': 'PreviousTeam2EndYear',
+                             'teamhist_3_teamId': 'PreviousTeam3ID',
+                             'teamhist_3_seasonStart': 'PreviousTeam3StartYear',
+                             'teamhist_3_seasonEnd': 'PreviousTeam3EndYear'}
+
+    player_profiles_df.rename(
+        columns=rename_column_mapping, inplace=True, errors='raise')
+
+    player_profiles_df = move_col(
+        player_profiles_df, ['PersonID', 'TeamID'], 'PlayerFirstName', 'Before')
+    player_profiles_df = move_col(
+        player_profiles_df, ['PlayerPositionName'], 'PlayerPositionCode', 'Before')
+
+    # Load
+    player_profiles_df.to_sql(name='player_profiles', con=connection,
+                              schema='staging', if_exists='replace', index=False, chunksize=100)
+
+
+# ETL DATA FROM "landing.player_career_stats" TO "staging.player_career_stats"
+with engine.connect() as connection:
+
+    # Extract
+    selected_columns = ['career_summary_tpp', 'career_summary_ftp', 'career_summary_fgp',
+                        'career_summary_ppg', 'career_summary_rpg', 'career_summary_apg',
+                        'career_summary_bpg', 'career_summary_mpg', 'career_summary_spg',
+                        'career_summary_assists', 'career_summary_blocks', 'career_summary_steals',
+                        'career_summary_turnovers', 'career_summary_offReb', 'career_summary_defReb',
+                        'career_summary_totReb', 'career_summary_fgm', 'career_summary_fga',
+                        'career_summary_tpm', 'career_summary_tpa', 'career_summary_ftm',
+                        'career_summary_fta', 'career_summary_pFouls', 'career_summary_points',
+                        'career_summary_gamesPlayed', 'career_summary_gamesStarted', 'career_summary_plusMinus',
+                        'career_summary_min', 'career_summary_dd2', 'career_summary_td3', 'personId']
+
+    player_career_stats_df = pd.read_sql_table(
+        table_name='player_career_stats', con=connection, schema='landing', columns=selected_columns)
+
+    # Transform
+    rename_column_mapping = {'career_summary_tpp': 'TPPercentage',
+                             'career_summary_ftp': 'FreeThrowPercentage',
+                             'career_summary_fgp': 'FieldGoalPercentage',
+                             'career_summary_ppg': 'PointsPerGameAverage',
+                             'career_summary_rpg': 'ReboundsPerGameAverage',
+                             'career_summary_apg': 'AssistsPerGameAverage',
+                             'career_summary_bpg': 'BlocksPerGameAverage',
+                             'career_summary_mpg': 'MinutesPerGameAverage',
+                             'career_summary_spg': 'StealsPerGameAverage',
+                             'career_summary_assists': 'TotalAssistsCount',
+                             'career_summary_blocks': 'TotalBlocksCount',
+                             'career_summary_steals': 'TotalStealsCount',
+                             'career_summary_turnovers': 'TotalTurnoversCount',
+                             'career_summary_offReb': 'TotalOffensiveReboundsCount',
+                             'career_summary_defReb': 'TotalDefensiveReboundsCount',
+                             'career_summary_totReb': 'TotalReboundsCount',
+                             'career_summary_fgm': 'TotalFieldGoalsMadeCount',
+                             'career_summary_fga': 'TotalFieldGoalsAttemptedCount',
+                             'career_summary_tpm': 'Total3-PointFieldGoalsMadeCount',
+                             'career_summary_tpa': 'Total3-PointFieldGoalsAttemptedCount',
+                             'career_summary_ftm': 'TotalFreeThrowsMadeCount',
+                             'career_summary_fta': 'TotalFreeThrowsAttemptedCount',
+                             'career_summary_pFouls': 'TotalPersonalFoulsCount',
+                             'career_summary_points': 'TotalPointsCount',
+                             'career_summary_gamesPlayed': 'TotalGamesPlayedCount',
+                             'career_summary_gamesStarted': 'TotalGamesStartedCount',
+                             'career_summary_plusMinus': 'PlusMinus',
+                             'career_summary_min': 'TotalMinutesPlayedCount',
+                             'career_summary_dd2': 'TotalDoubleDoublesCount',
+                             'career_summary_td3': 'TotalTripleDoublesCount',
+                             'personId': 'PersonID'}
+
+    player_career_stats_df.rename(columns=rename_column_mapping, inplace=True)
+
+    player_career_stats_df = move_col(player_career_stats_df, cols_to_move=[
+                                      'PersonID'], ref_col='TPPercentage', place='Before')
+
+    # Load
+    player_career_stats_df.to_sql(name='player_career_stats', con=connection,
+                                  schema='staging', if_exists='replace', index=False, chunksize=100)
+
+
 # landing_box_score_basic_game_stats
 # landing_box_score_detailed_game_stats
