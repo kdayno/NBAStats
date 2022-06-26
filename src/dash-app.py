f8fd73465c4063c218ed0ca6f81c3d696f3aa008
Kevin Dayno <kdayno@gmail.com>
2022-06-26 18:28:27 -0400

    Create header with pop out navigation menu
    - Header and pop out navigation built with dash mantine componenets
    - CSS updated to be consistent with NBA official website

--- src/dash-app.py
+++ src/dash-app.py
@@ -1,6 +1,8 @@
-from dash import Dash, html, dcc, dash_table
-import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc, dash_table, Output, Input, callback
+import dash_mantine_components as dmc
 import pandas as pd
+from dash_iconify import DashIconify
+
 
 df = pd.read_csv(
     '/Users/kdayno/Development/02-PROJECTS/NBAWarRoomDashboard/data/nba_season_2021_final_standings_v2.csv')
@@ -30,43 +32,172 @@ east_teams_df.sort_values(by='W', ascending=False, inplace=True)
 style_cell = {'height': '10px',
               'minWidth': '10px',
               'width': '250px',
-              'maxWidth': '250px',
+              'maxWidth': '200px',
               'font-family': 'arial',
-              'letterSpacing': '1px',
               'textAlign': 'center'}
 
-style_table = {'borderRadius': '5px',
+
+style_table = {'width': '350px',
                'overflow': 'hidden',
-               'border': '1px solid black',
-               'tableLayout': 'fixed',
-               'width': '350px'}
+               'borderRadius': '5px',
+               'border': '1px solid black'}
 
 
 app = Dash(__name__)
 
-app.layout = html.Div([
+# Row 1 > Navigation Bar
+# Row 2 > Table Visuals + Scatterplot Visual
+# Row 3 > Dropdown Menu for Table Visuals
 
-    html.Div([html.H1("NBA War Room Dashboard", className='banner')]),
 
-    html.Div([
-        html.Div(dash_table.DataTable(data=west_teams_df.to_dict('records'),
-                                      columns=[{"name": i, "id": i}
-                                               for i in west_teams_df.columns],
-                                      style_cell=style_cell,
-                                      style_table=style_table),
-             className='data-table left'
-                 ),
+def create_home_link(label):
+    return dmc.Text(
+        label,
+        size="xl",
+        style={"fontSize": 32, "color": "#FFFFFF"}
+    )
 
-        html.Div(dash_table.DataTable(data=east_teams_df.to_dict('records'),
-                                      columns=[{"name": i, "id": i}
-                                               for i in east_teams_df.columns],
-                                      style_cell=style_cell,
-                                      style_table=style_table),
-             className='data-table right'
-                 )
-    ],  className='table-container'),
 
-])
+app.layout = dmc.Grid(
+    children=[
+        dmc.Col(
+            dmc.Header(
+                class_name="dash-header",
+                height=75,
+                fixed=True,
+                p="md",
+                children=[
+                    dmc.Container(
+                        fluid=True,
+                        children=[dmc.Group(
+                            position="apart",
+                            align="flex-start",
+                            spacing="lg",
+                            children=[
+                                dmc.Group(
+                                    children=[
+                                        html.Img(src=app.get_asset_url(
+                                            'nba-logo-transparent.png'), style={'height': '50px', 'width': '25px'}),
+                                        dcc.Link(
+                                            [
+                                                dmc.MediaQuery(
+                                                    create_home_link(
+                                                        "NBA STATS Dashboard"),
+                                                    smallerThan="sm",
+                                                    styles={"display": "none"},
+                                                ),
+                                                dmc.MediaQuery(
+                                                    create_home_link(
+                                                        "NBA STATS"),
+                                                    largerThan="sm",
+                                                    styles={"display": "none"},
+                                                ),
+                                            ],
+                                            href='/',
+                                            style={"paddingTop": 5,
+                                                   "textDecoration": "none"},
+                                        ),
+                                    ]),
+                                dmc.Group(
+                                    children=[
+                                        dmc.Tooltip(
+                                            dmc.Button(
+                                                dmc.ThemeIcon(
+                                                    DashIconify(
+                                                        icon="dashicons:menu-alt",
+                                                        width=22,
+                                                        color="#FFFFFF",
+                                                    ),
+                                                    radius=30,
+                                                    size=36,
+                                                    variant="filled",
+                                                    style={
+                                                        "background-color": "#051B2D"}
+                                                ),
+                                                variant="subtle",
+                                                size="md",
+                                                class_name="menu-button",
+                                                id="drawer-button"
+                                            ),
+                                            label="Menu",
+                                            position="bottom",
+                                        ),
+                                        dmc.Drawer(
+                                            title="Drawer Menu",
+                                            id="drawer",
+                                            padding="md",
+                                            position="right"
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                        ]
+                    )
+                ]
+            ),
+            span=12,
+            style={"height": "120px"}),
+
+        dmc.Col(
+            dmc.Text("Regular Season Standings",
+                     style={"fontSize": 30,
+                            "textAlign": "center",
+                            "fontWeight": "bold"}
+                     ),
+            span=6,
+            offset=0.10,
+        ),
+
+        dmc.Col(
+            dmc.Text("Season At A Glance",
+                     style={"fontSize": 30,
+                            "textAlign": "center",
+                            "fontWeight": "bold"}
+                     ),
+            span=5
+
+        ),
+
+        dmc.Col(
+            html.Div(
+                dash_table.DataTable(data=west_teams_df.to_dict('records'),
+                                     columns=[{"name": i, "id": i}
+                                              for i in west_teams_df.columns],
+                                     style_cell=style_cell,
+                                     style_table=style_table)
+            ),
+            span=3,
+
+            style={"height": "250px"}
+        ),
+
+        dmc.Col(
+            html.Div(
+                dash_table.DataTable(data=east_teams_df.to_dict('records'),
+                                     columns=[{"name": i, "id": i}
+                                              for i in east_teams_df.columns],
+                                     style_cell=style_cell,
+                                     style_table=style_table)
+            ),
+            span=3,
+            # offset=0.1
+        ),
+
+        dmc.Col(
+            span=5.9
+        ),
+    ]
+)
+
+
+@ callback(
+    Output("drawer", "opened"),
+    Input("drawer-button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def drawer_menu(n_clicks):
+    return True
 
 
 if __name__ == '__main__':
